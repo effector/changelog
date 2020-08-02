0d1daf8dec1d2fbff03898ba846f5303325de70d
Zero Bias <zerobias@icloud.com>
2020-08-02 21:52:48 +0300

    use fork for static rendering

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import {resolve} from 'path'
 //@ts-ignore
 import {outputFile, copy} from 'fs-extra'
+import {fork} from 'effector'
 import {renderStatic} from 'forest/server'
 
-import {App} from './app'
+import {App, app, sections, versionDates} from './app'
+import {format} from 'prettier'
 import {fetchData} from './fetchData'
 
 async function generateStatic() {
-  const {sections, versionDates} = await fetchData()
+  const data = await fetchData()
 
-  const rendered = await renderStatic(() => {
-    App(sections, versionDates)
+  const scope = fork(app, {
+    values: new Map()
+      .set(sections, data.sections)
+      .set(versionDates, data.versionDates)
   })
+
+  const renderedRaw = await renderStatic({
+    scope,
+    fn: App
+  })
+
+  const rendered = format(renderedRaw, {parser: 'html', printWidth: 60})
 
   await Promise.all([
     outputFile(resolve(__dirname, 'index.html'), rendered),
