63ed861b6029e11eceaceda56166e5974de33cdf
Zero Bias <zerobias@icloud.com>
2020-08-14 04:51:09 +0300

    render full static page on server

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import {h, text, block, spec} from 'forest'
 import {renderStatic} from 'forest/server'
 
 import {
-  // Body,
+  Body,
   app,
   sections,
   versionDates,
@@ -25,12 +25,102 @@ const ClientScript = block({
   }
 })
 
+const HTMLHead = block({
+  fn() {
+    h('title', {
+      text: 'Effector changelog'
+    })
+    h('meta', {
+      attr: {charset: 'utf-8'}
+    })
+    Meta('viewport', {
+      content: [
+        'width=device-width',
+        // 'user-scalable=no',
+        'initial-scale=1',
+        // 'maximum-scale=1'
+        'viewport-fit=cover'
+      ].join(', ')
+    })
+    Meta('apple-mobile-web-app-capable', {
+      content: 'yes'
+    })
+    Meta('apple-mobile-web-app-status-bar-style', {
+      content: 'black'
+    })
+    Meta('apple-mobile-web-app-title', {
+      content: 'Effector changelog'
+    })
+    Meta('description', {
+      content:
+        'Changelog for effector, effector-react and effector-vue releases'
+    })
+    Meta('application-name', {
+      content: 'changelog'
+    })
+    Meta('theme-color', {
+      content: '#120309'
+    })
+    Meta('twitter:card', {
+      content: 'summary'
+    })
+    Meta('og:title', {
+      content: 'Effector changelog',
+      property: true
+    })
+    Meta('og:type', {
+      content: 'website',
+      property: true
+    })
+    Meta('og:url', {
+      content: 'https://changelog.effector.dev',
+      property: true
+    })
+    Meta('og:description', {
+      content:
+        'Changelog for effector, effector-react and effector-vue releases',
+      property: true
+    })
+    h('link', {
+      attr: {href: '/assets/src_styles_forest.css', rel: 'stylesheet'}
+    })
+    h('link', {
+      attr: {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: 'https://editor-prod.effector.dev/apple-touch-icon.png'
+      }
+    })
+    h('link', {
+      attr: {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: 'https://editor-prod.effector.dev/favicon-32x32.png'
+      }
+    })
+    h('link', {
+      attr: {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: 'https://editor-prod.effector.dev/favicon-16x16.png'
+      }
+    })
+    h('link', {
+      attr: {
+        rel: 'shortcut icon',
+        href: 'https://editor-prod.effector.dev/favicon.ico'
+      }
+    })
+  }
+})
+
 const App = block({
   fn() {
     h('html', () => {
       h('head', () => {
         HTMLHead()
-        ClientScript()
         h('script', {
           attr: {
             src: '/client.js',
@@ -38,140 +128,15 @@ const App = block({
             async: true
           }
         })
+        ClientScript()
       })
+
       h('body', () => {
-        // Body()
+        Body()
       })
     })
   }
 })
-
-async function generateStatic() {
-  console.log('start')
-  const data = await fetchData()
-
-  const scope = fork(app, {
-    values: new Map()
-      .set(sections, data.sections)
-      .set(versionDates, data.versionDates)
-  })
-
-  const serialized = serialize(scope, {
-    onlyChanges: true
-  })
-  await allSettled(setClientState, {
-    params: serialized,
-    scope
-  })
-
-  console.log('render')
-
-  const renderedRaw = await renderStatic({
-    scope,
-    fn: App
-  })
-
-  const rendered = format(renderedRaw, {parser: 'html', printWidth: 60})
-
-  console.log('output')
-  await Promise.all([
-    outputFile(resolve(__dirname, 'client', 'index.html'), rendered),
-    copy(
-      resolve(process.cwd(), 'src', 'assets'),
-      resolve(__dirname, 'client', 'assets')
-    )
-  ])
-  console.log('end')
-}
-
-generateStatic()
-
-function HTMLHead() {
-  h('title', {
-    text: 'Effector changelog'
-  })
-  h('meta', {
-    attr: {charset: 'utf-8'}
-  })
-  Meta('viewport', {
-    content: [
-      'width=device-width',
-      // 'user-scalable=no',
-      'initial-scale=1',
-      // 'maximum-scale=1'
-      'viewport-fit=cover'
-    ].join(', ')
-  })
-  Meta('apple-mobile-web-app-capable', {
-    content: 'yes'
-  })
-  Meta('apple-mobile-web-app-status-bar-style', {
-    content: 'black'
-  })
-  Meta('apple-mobile-web-app-title', {
-    content: 'Effector changelog'
-  })
-  Meta('description', {
-    content: 'Changelog for effector, effector-react and effector-vue releases'
-  })
-  Meta('application-name', {
-    content: 'changelog'
-  })
-  Meta('theme-color', {
-    content: '#120309'
-  })
-  Meta('twitter:card', {
-    content: 'summary'
-  })
-  Meta('og:title', {
-    content: 'Effector changelog',
-    property: true
-  })
-  Meta('og:type', {
-    content: 'website',
-    property: true
-  })
-  Meta('og:url', {
-    content: 'https://changelog.effector.dev',
-    property: true
-  })
-  Meta('og:description', {
-    content: 'Changelog for effector, effector-react and effector-vue releases',
-    property: true
-  })
-  h('link', {
-    attr: {href: '/assets/src_styles_forest.css', rel: 'stylesheet'}
-  })
-  h('link', {
-    attr: {
-      rel: 'apple-touch-icon',
-      sizes: '180x180',
-      href: 'https://editor-prod.effector.dev/apple-touch-icon.png'
-    }
-  })
-  h('link', {
-    attr: {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      href: 'https://editor-prod.effector.dev/favicon-32x32.png'
-    }
-  })
-  h('link', {
-    attr: {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      href: 'https://editor-prod.effector.dev/favicon-16x16.png'
-    }
-  })
-  h('link', {
-    attr: {
-      rel: 'shortcut icon',
-      href: 'https://editor-prod.effector.dev/favicon.ico'
-    }
-  })
-}
 
 function Meta(
   name: string,
@@ -188,3 +153,42 @@ function Meta(
     }
   })
 }
+
+async function generateStatic() {
+  console.log('start')
+  const data = await fetchData()
+  const scope = fork(app, {
+    values: new Map()
+      .set(sections, data.sections)
+      .set(versionDates, data.versionDates)
+  })
+
+  const serialized = serialize(scope, {
+    onlyChanges: true
+  })
+  await allSettled(setClientState, {
+    params: serialized,
+    scope
+  })
+
+  console.log('render')
+  const renderedRaw = await renderStatic({
+    scope,
+    fn: App
+  })
+
+  console.log('format')
+  const rendered = format(renderedRaw, {parser: 'html', printWidth: 120})
+
+  console.log('output')
+  await Promise.all([
+    outputFile(resolve(__dirname, 'client', 'index.html'), rendered),
+    copy(
+      resolve(process.cwd(), 'src', 'assets'),
+      resolve(__dirname, 'client', 'assets')
+    )
+  ])
+  console.log('end')
+}
+
+generateStatic()
