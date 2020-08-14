f7495a1094456ff06e8fdc8f3055dc4b405d2dc3
Zero Bias <zerobias@icloud.com>
2020-08-14 04:49:57 +0300

    use html hydration on client

--- src/client.ts
+++ src/client.ts
@@ -2,12 +2,14 @@ import {fork} from 'effector'
 import {using} from 'forest'
 import {app, Body} from './app'
 
-const scope = fork(app, {
-  values: (window as any).__INITIAL_STATE__
-})
+window.addEventListener('load', () => {
+  const scope = fork(app, {
+    values: (window as any).__INITIAL_STATE__
+  })
 
-using(document.body, {
-  scope,
-  fn: Body
-  // hydrate: true
+  using(document.body, {
+    scope,
+    fn: Body,
+    hydrate: true
+  })
 })
